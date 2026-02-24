62161d403449ad6c22a440479499f5165d2bba5c
Phạm Tuấn Kiệt <167632480+TuanKiet1774@users.noreply.github.com>
2026-02-24 09:32:18 +0700

    Chỉnh sửa lỗi

--- src/models/User.js
+++ src/models/User.js
@@ -100,12 +100,11 @@ const userSchema = new mongoose.Schema(
 userSchema.index({ username: "text", fullname: "text", email: "text", address: "text" });
 userSchema.index({ role: 1, isActive: 1 });
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
